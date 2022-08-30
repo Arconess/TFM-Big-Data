c1753d36b2b218b5d99efb63c399a81fc5630bf2
Pablo Arcones Puebla <pablo.arcones@gmail.com>
2022-08-30 19:51:38 +0200

    Initial repository structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sistema anti-copia para la detección del uso de herramientas de chat en tiempo real.',
+    author='Pablo Arcones Puebla',
+    license='',
+)
